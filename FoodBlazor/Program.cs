9abbca4c2239abd7b89903546b71784d3818306d
Sanjatul Hasan Siam <sanjatulhasansiam.iit@gmail.com>
2024-10-21 16:29:36 +0600

    CategoryList Component Created

--- FoodBlazor/Program.cs
+++ FoodBlazor/Program.cs
@@ -1,6 +1,8 @@
 using FoodBlazor.Components;
 using FoodBlazor.Components.Account;
 using FoodBlazor.Data;
+using FoodBlazor.Repository;
+using FoodBlazor.Repository.IRepository;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +12,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
