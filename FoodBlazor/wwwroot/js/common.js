d50bd49e7fd448bd68ed0796db575cc6cfaa97d0
Sanjatul Hasan Siam <sanjatulhasansiam.iit@gmail.com>
2024-10-21 19:24:24 +0600

    Category Functionality - delet and modal completed

---
+++ FoodBlazor/wwwroot/js/common.js
@@ -0,0 +1,6 @@
+﻿function ShowConfirmationModal() {
+    bootstrap.Modal.getOrCreateInstance(document.getElementById("bsConfirmationModal")).show();
+}
+function HideConfirmationModal() {
+    bootstrap.Modal.getOrCreateInstance(document.getElementById("bsConfirmationModal")).hide();
+}
