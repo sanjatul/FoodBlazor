65734d3c2d5f5381100f46516476212979a910ed
Sanjatul Hasan Siam <sanjatulhasansiam.iit@gmail.com>
2024-10-21 17:47:57 +0600

    Toaster Notification added on successful Updataion andd deletion of category

---
+++ FoodBlazor/Services/Extensions/IJSRuntimeExtension.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.JSInterop;
+
+namespace FoodBlazor.Services.Extensions
+{
+    public static class IJSRuntimeExtension
+    {
+        public static async Task ToastrSuccess(this IJSRuntime js,string message)
+        {
+            await js.InvokeVoidAsync("ShowToaster", "success", message);
+        }
+
+        public static async Task ToastrError(this IJSRuntime js, string message)
+        {
+            await js.InvokeVoidAsync("ShowToaster", "message", message);
+        }
+    }
+}
